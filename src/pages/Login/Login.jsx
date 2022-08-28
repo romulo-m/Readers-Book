68328a11ffc5c0b418a6c0618f70c899c1afc17b
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-08-28 16:52:36 -0300

    criei os componetes input e label

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import S from "./Login.module.css"
+
+const Login = () => {
+  return (
+    <form>
+
+
+    </form>
+  )
+}
+
+export default Login
