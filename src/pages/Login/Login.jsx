12d31db441d147f4ccc9f8b7cd130d0b042b43eb
andre-sqr <andrebsqr@gmail.com>
2022-08-31 13:38:09 -0300

    fazendo tela de login com responsividade

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,18 +3,24 @@ import Button from '../../components/Button/Button'
 import Input from '../../components/Input/Input'
 import Label from '../../components/Label/Label'
 import S from "./Login.module.css"
+import bookshelf from '../../assets/bookshelf.jpg'
 
 const Login = () => {
   return (
     <section className={S.container}>
-      <h2>Área de login</h2>
       <form className={S.forms}>
+        <h1>Login</h1>
+        <div className={S.inputsection}>
         <Label style={S.label} texto={'Usuário'}/>
-        <Input style={S.input} type={'text'} placeholder={'Digite seu usuário'}/>
-        <Label style={S.label} texto={'Digite sua senha'}/>
+        <Input style={S.input} type={'text'}/>
+        </div>
+        <div className={S.inputsection}>
+        <Label style={S.label} texto={'Senha'}/>
         <Input style={S.input} type={'text'} placeholder={'********'}/>
+        </div>
         <Button style={S.btn} texto={'Entrar'} />
       </form>
+      <img src={bookshelf} className={S.img}></img>
     </section>
   )
 }
