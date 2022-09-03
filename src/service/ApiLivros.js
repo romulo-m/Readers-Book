a4cc56ddf7d7d9ac5b8e8283f9e549b1b9a4e5a9
Broad <romulo.mgaudencio@gmail.com>
2022-09-03 14:51:11 -0300

    api get

--- src/service/ApiLivros.js
+++ src/service/ApiLivros.js
@@ -4,6 +4,12 @@ export const api = axios.create({
     baseURL: 'https://olimpia-api.herokuapp.com',
   });
 
+  export const estoqueGet = async (res) => {
+    const response = await api.post('/estoque', res)
+       return response.data.produtos
+       ;
+   }
+
   export const livrosPost = async (res) => {
    const response = await api.post('/livros', res)
       return response.data.livros
