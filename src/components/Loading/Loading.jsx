99564c09e4f6eb9eada007408bee87471c781e31
joao-pedro-de-moura <jpmouraj@gmail.com>
2022-09-07 05:49:48 -0300

    estilização do componente loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import S from './loading.module.css'
 const Loading = () => {
   return (
-    <div >
+    <div className ={S.body} >
          <div className={S.spinner}></div>
     </div>
   )
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -10,96 +10,66 @@ import Loading from '../../components/Loading/Loading'
 
 const Table = () => {
     const [req, setReq] = useState(0)
-    const [removeLoading, setRemoveloading] = useState(false)
+    const [removeLoading, setRemoveloading] = useState(true)
     const [table, setTable] = useState([])
-
-    // const results = table.filter((item) => {
-    //     if (value === '') {
-    //         return item;
-    //     } else if (item.titulo.toLowerCase().includes(value.toLowerCase())) {
-    //         return item;
-    //     }
-    // });
+    const [idLivro, setIdLivro] = useState()
+    const [openDelete, setOpenDelete] = useState(false);
+    
+    
+    
 
     async function getLivros() {
         try {
             const response = await estoqueGet()
-            setTable(response)
+            setTimeout(() => {
+                setRemoveloading(true)
+                setTable(response)
+            },3000)
             
         } catch (e) {
             return e.message
         }
         
     }
-
-    // async function atualizarLivro(id) {
-    //     try {
-    //         const response = await updateBook()
-    //         console.log(response)
-    //     } catch (e) {
-    //         return e.message
-    //     }
-    // }
-
+    
+    
     async function deletarLivro() {
         try {
             const response = await livrosDelete(idLivro)
             console.log(response)
             fecharModal()
             
+            
             setReq(req + 1)
         } catch (e) {
             return e.message
         }
     }
-
+    
+    
+    
+    function abrirModal(id) {
+        setIdLivro(id)
+        setOpenDelete(true);
+    } 
+    
+    
+    function fecharModal() {
+        setOpenDelete(false);
+    }
+    
+    
+    
     useEffect(() => {
-        setRemoveloading(true)
-    },[req])
-
+        setRemoveloading(false)
+    },[])
+    
     useEffect(() => {
         getLivros()
     },[req])
     
-  const [idLivro, setIdLivro] = useState()
-  const [openDelete, setOpenDelete] = useState(false);
-
-  function abrirModal(id) {
-    setIdLivro(id)
-    setOpenDelete(true);
-  }
-
-  function fecharModal() {
-    setOpenDelete(false);
-  }
-
-    // function renderRows() {
-    //     return table && table.map((item, index) => {
-    //         return (
-    //             <tr key={index}>
-    //                 <td className={S.linha}>{item.idLivro}</td>
-    //                 <td className={S.linha}>{item.titulo}</td>
-    //                 <td className={S.linha}>{item.formato}</td>
-    //                 <td className={S.linha}>R$ {item.valor},00</td>
-    //                 <td className={S.linha}>
-    //                 <Link 
-    //                     className={S.btn}
-    //                     role="button"
-    //                     to = {`/update/${item.idLivro}`}
-    //                     >
-
-    //                     <button className={S.btn}><RiEdit2Fill /></button>
-    //                 </Link>
-                        
-    //                     <button className={S.btn} onClick={abrirModal(item.idLivro)}><RiDeleteBin6Fill /></button>
-    //                 </td>
-    //             </tr>
-    //         )
-    //     })
-    // }
-
-  return (
-    <table className={S.table}>
+    return (
+        <table className={S.table}>
         <thead>
             <tr>
                 <th className={S.titulo}>ID</th>
@@ -110,9 +80,10 @@ const Table = () => {
             </tr>
         </thead>
         <tbody>
+            
             {table && table.map((item, index) => {
-            return (
-                <tr key={index}>
+                return (
+                    <tr key={index}>
                     <td className={S.linha}>{item.idLivro}</td>
                     <td className={S.linha}>{item.titulo}</td>
                     <td className={S.linha}>{item.formato}</td>
@@ -130,9 +101,9 @@ const Table = () => {
                         <button className={S.btn} onClick={()=>{
                             abrirModal(item.idLivro)
                         }
-                          
-                                      
-                        }><RiDeleteBin6Fill /></button>
+                        
+                        
+                    }><RiDeleteBin6Fill /></button>
                     </td>
                 </tr>
             )
@@ -140,17 +111,63 @@ const Table = () => {
         </tbody>
         <ModalDeleteLivros
         open={openDelete}
-         onClose={fecharModal}
-         deletaLivros={deletarLivro}
-         idLivro={idLivro}
-
+        onClose={fecharModal}
+        deletaLivros={deletarLivro}
+        idLivro={idLivro}
+        
         />
-    {!removeLoading && <Loading/>}
+        <div className = {S.divLoading} >
+            {!removeLoading && <Loading/>}
+        </div>
     </table>
     
-  )
+    )
 }
 
 export default Table
 
 
+
+// function renderRows() {
+    //     return table && table.map((item, index) => {
+        //         return (
+            //             <tr key={index}>
+            //                 <td className={S.linha}>{item.idLivro}</td>
+            //                 <td className={S.linha}>{item.titulo}</td>
+            //                 <td className={S.linha}>{item.formato}</td>
+            //                 <td className={S.linha}>R$ {item.valor},00</td>
+            //                 <td className={S.linha}>
+            //                 <Link 
+            //                     className={S.btn}
+            //                     role="button"
+            //                     to = {`/update/${item.idLivro}`}
+            //                     >
+            
+            //                     <button className={S.btn}><RiEdit2Fill /></button>
+            //                 </Link>
+            
+            //                     <button className={S.btn} onClick={abrirModal(item.idLivro)}><RiDeleteBin6Fill /></button>
+            //                 </td>
+            //             </tr>
+            //         )
+            //     })
+            // }
+            
+            
+            // const results = table.filter((item) => {
+            //     if (value === '') {
+            //         return item;
+            //     } else if (item.titulo.toLowerCase().includes(value.toLowerCase())) {
+            //         return item;
+            //     }
+            // });
+                
+
+            // async function atualizarLivro(id) {
+            //     try {
+            //         const response = await updateBook()
+            //         console.log(response)
+            //     } catch (e) {
+            //         return e.message
+            //     }
+            // }
