e81b62fa680038709b1a3280343942d200393084
BeatrizMioranza <beatrizmioranza2508@gmail.com>
2022-09-08 00:42:12 -0300

    feat: estilizando a page home, adicionei o cards

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,26 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import Label from "../../components/Label/Label";
 import S from "./Login.module.css";
 import bookshelf from "../../assets/bookshelf.jpg";
 import UserContext from "../../contexts/UserContext";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
-const initialState= { user: "", password: "" };
+const initialState = { user: "", password: "" };
 
-function acessLogin({user, password}){
-  console.log(user)
-  if(user=== "admin" && password === "123456"){
-    return true
-  } else{ 
-    return false
+function acessLogin({ user, password }) {
+  if (user === "admin" && password === "123456") {
+    return true;
+  } else {
+    return false;
   }
 }
 const Login = () => {
   const [values, setValues] = useState(initialState);
-  const {setToken} = useContext(UserContext)
-  const navigate = useNavigate()
+  const { setToken } = useContext(UserContext);
+  const navigate = useNavigate();
   function onChange(event, name) {
     const { value } = event.target;
     setValues({
@@ -29,31 +29,29 @@ const Login = () => {
     });
   }
 
-  function submit(event){
-    event.preventDefault()
-    const token = acessLogin(values); 
-    console.log(token)
-    if(token){
-      // setToken(token);
-      navigate('/dashboard')
-    }else {
-      setValues(initialState)
+  function submit(event) {
+    event.preventDefault();
+    const token = acessLogin(values);
+    console.log(token);
+    if (token) {
+      navigate("/home");
+    } else {
+      setValues(initialState);
     }
-  };
-
-
+  }
 
   return (
     <section className={S.container}>
-      <form className={S.forms} >
-        <h1>Login</h1>
+      <form className={S.forms}>
+        <h1>Reader's Book</h1>
+        <h3>Entre com seu usuário e senha</h3>
         <div className={S.inputsection}>
           <Label style={S.label} texto={"Usuário"} />
           <Input
             style={S.input}
             type={"text"}
             name={"user"}
-            onChange={(event)=>onChange(event, "user")}
+            onChange={(event) => onChange(event, "user")}
             defaultValue={values.user}
           />
         </div>
@@ -64,11 +62,11 @@ const Login = () => {
             type={"password"}
             placeholder={"********"}
             name={"password"}
-            onChange={(event)=>onChange(event, "password")}
+            onChange={(event) => onChange(event, "password")}
             defaultValue={values.password}
           />
         </div>
-        <Button style={S.btn} texto={"Entrar"} onClick={submit}/>
+        <Button style={S.btn} texto={"Entrar"} onClick={submit} />
       </form>
       <img src={bookshelf} className={S.img}></img>
     </section>
